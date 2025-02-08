2b10bdfe68f7c594e85866a8886327aad7c17d78
stevebiczyk <stevebiczyk@gmail.com>
2025-02-08 13:35:26 +0000

    navigating programatically

--- src/app/products/[productId]/reviews/[reviewId}/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId}/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function ProductReview({
   params,
@@ -7,7 +7,8 @@ export default async function ProductReview({
 }) {
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
-    return notFound();
+    // notFound();
+    redirect("/products");
   }
   return (
     <h1 className="title" style={{ fontSize: "2rem" }}>
