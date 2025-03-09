1db9059701e71e83ef5a02d98092f4407534b7ef
stevebiczyk <stevebiczyk@gmail.com>
2025-03-09 23:48:08 +0000

    lesson 31 - unmatched routes

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,15 @@
 import { Card } from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
-  return <Card>Nitifications</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <div>
+        <Link href="/complex-dashboard/archived">
+          <a>Archived</a>
+        </Link>
+      </div>
+    </Card>
+  );
 }
---
+++ src/app/complex-dashboard/@users/default.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/card";
+
+export default function UserAnalytics() {
+  return (
+    <Card>
+      <div>User analytics default content</div>
+    </Card>
+  );
+}
